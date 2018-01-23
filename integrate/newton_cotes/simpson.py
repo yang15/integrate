0d7f8b6bea29f9f1112225aa6e9e589affa9ab50
yang15 <yang15@sas.upenn.edu>
2018-01-23 13:46:32 -0500

    modified newton_cotes-simpson.py with floating numbers

--- integrate/newton_cotes/simpson.py
+++ integrate/newton_cotes/simpson.py
@@ -8,8 +8,8 @@ def evaluate(x, f):
     a = x[0]
     b = x[1]
     ya = f(a)
-    yb = f((a+b)/2)
+    yb = f((a+b)/2.0)
     yc = f(b)
-    I = (b-a) * (ya + 4 * yb + yc) / 6
+    I = (b-a) * (ya + 4.0 * yb + yc) / 6.0
     return I
 
