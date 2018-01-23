0918eafbb3b65c3cf19a182a9a5baa27d1fb2f6e
yang15 <yang15@sas.upenn.edu>
2018-01-23 14:17:41 -0500

    format changing

--- integrate/newton_cotes/simpson.py
+++ integrate/newton_cotes/simpson.py
@@ -4,12 +4,12 @@ This file contains the implementation of the Simpson rule
 
 import numpy as np
 
+
 def evaluate(x, f):
     a = x[0]
     b = x[1]
     ya = f(a)
-    yb = f((a+b)/2.0)
+    yb = f((a + b) / 2.0)
     yc = f(b)
-    I = (b-a) * (ya + 4.0 * yb + yc) / 6.0
+    I = (b - a) * (ya + 4.0 * yb + yc) / 6.0
     return I
-
